3e22d11b46dc734051e4408ef61befa8daf20f5f
DorottyaVarju <varjudorottya@gmail.com>
2024-03-04 20:03:46 +0100

    started to work on routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import './components/WordToGuess.js';
 import WordToGuess from './components/WordToGuess.js';
