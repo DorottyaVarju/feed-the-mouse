289f5142df5f7b5515f562ba071a1dfe55f3b52c
DorottyaVarju <varjudorottya@gmail.com>
2024-03-30 15:59:34 +0100

    change from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import CategoryAndNumOfLettersToChoose from './components/CategoryAndNumOfLettersToChoose.js';
 import WordToGuess from './components/WordToGuess.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-      <Routes>
-        <Route path="/game" element={<WordToGuess />} />
-        <Route path="/" element={<CategoryAndNumOfLettersToChoose />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route exact path="/game" component={<WordToGuess/>} />
+          <Route exact path="/" component={<CategoryAndNumOfLettersToChoose/>} />
+        </Routes>
+      </HashRouter>
     </div>
-    </BrowserRouter>
   );
 }
 
