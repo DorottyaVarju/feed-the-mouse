07de6540be635b3aa9ccc936c66e0784a79139ae
DorottyaVarju <varjudorottya@gmail.com>
2024-09-01 16:38:29 +0200

    img, style changes

--- src/components/HangmanDisplay.js
+++ src/components/HangmanDisplay.js
@@ -1,89 +1,72 @@
 import React from "react";
 import '../css/HangmanDisplay.css';
-import { useRef, useEffect } from "react";
+import {useEffect } from "react";
 import cheese from '../images/cheese.png';
+import ladderfoot from '../images/ladderfoot.png';
+import rung from '../images/rung.png';
 import mouse from '../images/mouse.png';
 import yescheese from '../images/yescheese.png';
 
 function HangmanDisplay({wrongGuess}){
-    const canvasRef = useRef(null);
+    if(document.getElementById("ladderfootleft") !== null){
+        document.getElementById("ladderfootleft").style.opacity = "0";
+        document.getElementById("ladderfootright").style.opacity = "0";
+        document.getElementById("rung5").style.opacity = "0";
+        document.getElementById("rung4").style.opacity = "0";
+        document.getElementById("rung3").style.opacity = "0";
+        document.getElementById("rung2").style.opacity = "0";
+        document.getElementById("rung1").style.opacity = "0";
+    }
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        ctx.translate(0.5, 0.5);
 
-        ctx.strokeStyle = 'white';
-        ctx.lineCap = 'round';
-        ctx.lineWidth = 2.5;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // left leg of the ladder
         if (wrongGuess > 0) {
-            ctx.beginPath();
-            ctx.moveTo(10, 210);
-            ctx.lineTo(40, 35);
-            ctx.stroke();
+            document.getElementById("ladderfootleft").style.opacity = "1";
         }
 
-        // right leg of the ladder
         if (wrongGuess > 1) {
-            ctx.beginPath();
-            ctx.moveTo(50, 210);
-            ctx.lineTo(80, 35); 
-            ctx.stroke();
+            document.getElementById("ladderfootright").style.opacity = "1";
         }
 
-        // rung
         if (wrongGuess > 2) {
-            ctx.beginPath();
-            ctx.moveTo(5, 180);
-            ctx.lineTo(62.5, 180);
-            ctx.stroke();
+            document.getElementById("rung5").style.opacity = "1";
         }
 
-        // rung
         if (wrongGuess > 3) {
-            ctx.beginPath();
-            ctx.moveTo(10, 150);
-            ctx.lineTo(65, 150);
-            ctx.stroke();
+            document.getElementById("rung4").style.opacity = "1";
         }
 
-        // rung
         if (wrongGuess > 4) {
-            ctx.beginPath();
-            ctx.moveTo(15, 120);
-            ctx.lineTo(72.5, 120);
-            ctx.stroke();
+            document.getElementById("rung3").style.opacity = "1";
         }
 
-        // rung
         if (wrongGuess > 5) {
-            ctx.beginPath();
-            ctx.moveTo(20, 90);
-            ctx.lineTo(77.5, 90);
-            ctx.stroke();
+            document.getElementById("rung2").style.opacity = "1";
         }
 
-        // rung
         if (wrongGuess > 6) {
-            ctx.beginPath();
-            ctx.moveTo(25, 60);
-            ctx.lineTo(82.5, 60);
-            ctx.stroke();
+            document.getElementById("rung1").style.opacity = "5";
         }
-
-        ctx.translate(-0.5, -0.5);
 
     }, [wrongGuess]);
 
     return (
-        <div id="canvasDiv">
-            <img className="mouse" id="mouse" src={wrongGuess > 6 ? yescheese : mouse} title="mouse"></img>
-            <canvas ref={canvasRef} height="220" width="100"></canvas>
-            <img className="cheese" src={cheese} title="cheese"></img>
+        <div id="drawingDiv">
+            <img className="mouse" id="mouse" alt="mouse" src={wrongGuess > 6 ? yescheese : mouse} title="mouse"></img>
+            <div id="ladder" style={{display: 'flex', height: "100%", marginTop: "-20px"}}>
+                <div style={{display: 'flex', position: "absolute", height: "100%"}}>
+                    <img style={{height:'300px', width:'180px', marginRight:'-40px', opacity:'0'}} className="ladderfoot" id="ladderfootleft" alt="ladderfootleft" src={ladderfoot} title="ladderfootleft"></img>
+                    <img style={{height:'300px', width:'180px', marginLeft:'-60px', opacity:'0'}} className="ladderfoot" id="ladderfootright" alt="ladderfootright" src={ladderfoot} title="ladderfootright"></img>
+                </div>
+                <div style={{display: 'flex', flexDirection:"column", height: '100%', position: "relative"}}>
+                    <img style={{height:'360px', width:'195px', marginBottom: '-310px', marginLeft: '50px',marginTop: '-115px', opacity:'0'}} className="rung" id="rung1" alt="rung1" src={rung} title="rung"></img>
+                    <img style={{height:'360px', width:'195px', marginBottom: '-310px', marginLeft: '40px', opacity:'0'}} className="rung" id="rung2" alt="rung2" src={rung} title="rung"></img>
+                    <img style={{height:'360px', width:'195px', marginBottom: '-310px', marginLeft: '30px', opacity:'0'}} className="rung" id="rung3" alt="rung3" src={rung} title="rung"></img>
+                    <img style={{height:'360px', width:'195px', marginBottom: '-310px', marginLeft: '25px', opacity:'0'}} className="rung" id="rung4" alt="rung4" src={rung} title="rung"></img>
+                    <img style={{height:'360px', width:'195px', marginBottom: '-310px', marginLeft: '15px', opacity:'0'}} className="rung" id="rung5" alt="rung5" src={rung} title="rung"></img>
+                </div>
+            </div>
+            <img className="cheese" src={cheese} alt="cheese" title="cheese"></img>
         </div>
     );
 }
--- src/components/LettersToTry.js
+++ src/components/LettersToTry.js
@@ -1,81 +1,100 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../css/LettersToTry.css';
 import nocheese from '../images/nocheese.png';
 
 function LettersToTry(props) {
+    const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const { word, wrongGuess, onWrongLetter } = props;
+    const letterAboveLine = document.getElementsByClassName('letterAboveLine');
+    const handleKeyUp = (event) => {
+        const letter = event.key.toUpperCase();
+        if (abc.includes(letter)) {
+            isThisLetterInTheWord(letter);
+        }
+    };
 
-    let word = props.word;
-    let wrongGuess = props.wrongGuess;
-    let onWrongLetter = props.onWrongLetter;
-    let letterAboveLine = document.getElementsByClassName('letterAboveLine');
-    
-    const elementsToReturn = abc.map((item, index) => {
-        const handleClick = () => isThisLetterInTheWord(item);
-        return (
-          <button type="button" id={item} className="letters" key={index} onClick={handleClick}>
+    useEffect(() => {
+
+        document.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            document.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [word, wrongGuess, onWrongLetter]);
+
+    const handleClick = (item) => () => {
+        isThisLetterInTheWord(item);
+    };
+
+    const elementsToReturn = abc.map((item, index) => (
+        <button
+            type="button"
+            id={item}
+            className="letters"
+            key={index}
+            tabIndex="0"
+            onClick={handleClick(item)}
+        >
             {item}
-          </button>
-        );
-    });
-      
+        </button>
+    ));
+
     const unTriedLetter = () => {
         document.getElementById('mark').style.opacity = 1;
         Array.from(document.getElementsByClassName('letters')).forEach((letter) => {
             letter.classList.add('untriedLetter');
         });
         document.getElementById('mouse').src = nocheese;
-    }
-    
+        document.removeEventListener('keyup', handleKeyUp);
+    };
+
     function isThisLetterInTheWord(letterOfAbc) {
         let letterFound = false;
         let letterOfAbcElement = document.getElementById(letterOfAbc);
-        (Array.from(word)).forEach((letterOfWord, indexOfWord) => {
+
+        Array.from(word).forEach((letterOfWord, indexOfWord) => {
             if (letterOfWord === letterOfAbc) {
                 letterFound = true;
-                (Array.from(letterAboveLine)).forEach((line, indexOfLine) => {
+                Array.from(letterAboveLine).forEach((line, indexOfLine) => {
                     if (indexOfLine === indexOfWord) {
                         line.innerText = letterOfWord;
-                        if (!(letterOfAbcElement.classList.contains('alreadyInWordLetter'))) {
+                        if (!letterOfAbcElement.classList.contains('alreadyInWordLetter')) {
                             letterOfAbcElement.classList.add('alreadyInWordLetter');
                         }
                     }
                 });
 
-                if ((document.querySelectorAll('.alreadyInWordLetter')).length === word.length) {
+                if (document.querySelectorAll('.alreadyInWordLetter').length === word.length) {
                     unTriedLetter();
                 } else {
                     const indexes = [];
-                    (Array.from(document.querySelectorAll('.alreadyInWordLetter'))).forEach((charElement) => {
+                    Array.from(document.querySelectorAll('.alreadyInWordLetter')).forEach((charElement) => {
                         const char = charElement.textContent;
                         let index = -1;
-                        
                         do {
-                          index = word.indexOf(char, index + 1);
-                          if (index !== -1) {
-                            indexes.push(index);
-                          }
+                            index = word.indexOf(char, index + 1);
+                            if (index !== -1) {
+                                indexes.push(index);
+                            }
                         } while (index !== -1);
+                    });
 
-                    }); 
-
-                    if(indexes.length === word.length){
+                    if (indexes.length === word.length) {
                         unTriedLetter();
                     }
                 }
-
             }
         });
 
         if (!letterFound && onWrongLetter) {
             onWrongLetter();
             letterOfAbcElement.classList.add('wrongLetterGuess');
-            console.log(wrongGuess);
-            if(wrongGuess>5) {
+            //            console.log(wrongGuess);
+            if (wrongGuess > 5) {
                 unTriedLetter();
-                (Array.from(word)).forEach((letterOfWord,indexOfWord) => {
-                    (Array.from(letterAboveLine)).forEach((line, indexOfLine) => {
+                Array.from(word).forEach((letterOfWord, indexOfWord) => {
+                    Array.from(letterAboveLine).forEach((line, indexOfLine) => {
                         if (indexOfLine === indexOfWord) {
                             line.innerText = letterOfWord;
                         }
@@ -86,7 +105,6 @@ function LettersToTry(props) {
     }
 
     return <div id="elementsToReturn">{elementsToReturn}</div>;
-
 }
 
 export default LettersToTry;
